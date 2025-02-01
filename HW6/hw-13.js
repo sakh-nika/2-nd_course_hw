26ac0e0ae790bdff57773925556f7c4a3581bbf1
sakh-nika <you@example.com>
2025-02-01 11:52:39 +1100

    Changed the file hw-13.js

--- HW6/hw-13.js
+++ HW6/hw-13.js
@@ -1,13 +1,13 @@
 //задание 1 Создайте цикл, который будет выводить элементы массива, пока не встретит значение 10
 const numbers = [1, 5, 4, 10, 0, 3]
 for (let i = 0; i < numbers.length; i++) {
-    if (numbers[i] == 10) break;
     console.log(numbers[i]);
+    if (numbers[i] === 10) break; 
 }
 
 // задание 2 Найдите индекс значения 4 в массиве
 numbers.forEach((i, index) => {
-    if (i == 4) {
+    if (i === 4) {
         console.log(`У ${i} индекс ${index}`);
     }
 });
@@ -35,8 +35,7 @@ console.log(numb);
 
 // задание 6 С помощью метода sort отсортируйте массив и удалите букву 'a' из массива
 numb = [9, 8, 7, 'a', 6, 5]
-numb.sort();
-numb.pop('a');
+numb = numb.filter(item => typeof item === 'number').sort((a, b) => a - b);
 console.log(numb);
 
 //задание 7 
@@ -76,42 +75,37 @@ console.log(newNumb);
 //задание 10 Массив с произвольными числами, переберите его с помощью цикла for, В каждой итерации выведите в консоль сумму текущего и следующего элементов массива
 
 numb = [1, 8, 6, 4, 7, 3, 5, 2, 10, 9]
-for (let i = 0; i < numb.length; i++) { 
+for (let i = 0; i < numb.length - 1; i++) { 
     //let nextI = numb[i + 1]; // переменная для след числа
     let sum = (numb[i] + numb[i + 1]);
     //console.log(`${numb[i]} + ${numb[i + 1]}`); //выводит арифм действие
-    if (isNaN(sum)) break;
     console.log(sum);
 }
 
 //задание 11 Создайте функцию, которая принимает на вход массив целых чисел, а возвращает массив квадратов этих чисел.
 let getSquared = (numb) => {
-    const squaredNumb = numb.map(numb => numb ** 2); // coздаем новый массив с измененными значениями
-    console.log(squaredNumb);
+    return numb.map(numb => numb ** 2); // coздаем новый массив с измененными значениями
 }
-getSquared([2, 3, 4, 5, 6]);
+console.log(getSquared([2, 3, 4, 5, 6]));
 
 //задание 12 Создайте функцию, которая принимает на вход массив строк, а возвращает массив длины слов
 let getStringLength = (string) => {
-    const lengthString = string.map(string => string.length);
-    console.log(lengthString);
+    return string.map(string => string.length);
+    
 }
-getStringLength([`январь`, `февраль`, `март`]); // [6, 7, 4]
+console.log(getStringLength([`январь`, `февраль`, `март`])); // [6, 7, 4]
 
 //задание 13 Создайте функцию, которая принимает на вход массив целых чисел, а возвращает массив, содержащий только отрицательные значения.
 let getNegative = (numb) => {
-    const negativeNumb = numb.filter(numb => numb < 0);
-    console.log(negativeNumb);
+    return numb.filter(numb => numb < 0);
 }
-getNegative([-1, 2, -3, 8, -6, 7 ]);
+console.log(getNegative([-1, 2, -3, 8, -6, 7 ]));
 
 //задание 14 Создайте массив из 10 значений с помощью метода Math.random() в диапазоне от 0 до 10. Найдите все четные значения и добавьте их в новый массив.
 
 numb = []
    for (let n = 0; n < 10; n++ ) {
-    max = 10;
-    min = 0;
-    numb[n] = Math.floor(Math.random() * (max - min + 1)) + min;
+    numb[n] = Math.floor(Math.random() * (10 + 1)) ;
    }
    const evenNumb = numb.filter(numb => numb % 2 === 0);
    console.log(numb);
@@ -121,14 +115,15 @@ numb = []
 
 numb = []
     for (let n = 0; n < 6; n++ ) {
-    max = 10;
-    min = 0;
-    numb[n] = Math.floor(Math.random() * (max - min + 1)) + min;
+    numb[n] = Math.floor(Math.random() * 10) + 1;
     }
-   let sum = numb.reduce((total, numb) => total + numb, 0);
-   let arithmeticMean = sum / numb.length;
-    //let arithmeticMean = (numb.reduce((total, numb) => total + numb, 0)) / numb.length;
-
+    let sum = numb.reduce((total, numb) => total + numb, 0); // сумма всех чисел в массиве
+    let arithmeticMean = sum / numb.length;
     console.log(numb);
-    //console.log(sum);
     console.log(`Среднее арифметическое равно ${arithmeticMean}`);
+
+    // или
+const numbFrom = Array.from({length: 6}, () => Math.floor(Math.random() * 10) + 1);
+console.log(numb, numb.reduce((sum, n) => sum + n, 0) / numb.length);
+
+
